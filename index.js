12e4c204af1262032de42ab9bef7c428cc3136dc
Alfredo Espinosa <alfredo.espinosa@enroutesystems.com>
2018-02-07 09:16:44 -0700

    parametrize keyword

--- index.js
+++ index.js
@@ -9,56 +9,14 @@ var cron = require('node-cron');
 var db = require('./fb_db');
 
 // Define the token
-const token = ':taco:';
-const regex_token = new RegExp(':taco:', "g");
+const token = config.bot_params.keyword;
+const regex_token = new RegExp(config.bot_params.keyword, "g");
 const regex_mention = new RegExp('<@.*>', "g");
 
 // Setup CRON to reset coins
 cron.schedule('59 23 * * ' + config.schedule.days, function() {
   db.resetCoins();
 });
-
-// Listener for direct messages
-// controller.hears(token, 'direct_message', function(bot, message) {
-//
-//   try {
-//     // Get mentioned users
-//     var mentioned_users = message.text.match(regex_mention);
-//
-//     // Get number of tokens
-//     var coins = message.text.match(regex_token).length;
-//
-//     // Get only the first mentioned user
-//     var mention_user = mentioned_users[0].split(' ')[0];
-//
-//     // Get the user ID
-//     var mention_id = mentioned_users[0].split(' ')[0].replace(/<|@|>/g, '');
-//
-//     // Check if there are mentioned users AND token AND mentioned user is not
-//     // the same as current user
-//
-//     if (mentioned_users.length != 0 && coins !=0 && message.user != mention_id) {
-//
-//       db.getCurrentCoins(message.user).then(function(snapshot) {
-//
-//         if (snapshot.val() < coins) {
-//           bot.reply(message, 'No tienes suficientes tokens prro');
-//         }
-//
-//       }).then(function() {
-//
-//         db.processExchange(message.user, mention_id, coins);
-//
-//       });
-//
-//     }
-//
-//   }
-//   catch(err) {
-//     console.warn('Error giving reward.' + err);
-//   }
-//
-// });
 
 // Listener for TOKEN channel messages
 controller.hears(':taco:', 'ambient', function(bot, message) {
@@ -98,7 +56,7 @@ controller.hears(':taco:', 'ambient', function(bot, message) {
 });
 
 // Listener for LEADERBOARD channerl messages
-controller.hears('show', 'direct_message', function(bot,message) {
+controller.hears('show leaderboard', 'ambient', function(bot,message) {
 
   var users = [];
   var leaderboard = [];
