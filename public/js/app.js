9d2a6158993b50084daf0cf796e750ccdb1d2488
Rodrigo Rodriguez <rodrigo.rodriguez@enroutesystems.com>
2018-12-19 16:21:51 -0600

    UI-8: Fix new reward

--- public/js/app.js
+++ public/js/app.js
@@ -70,8 +70,11 @@ app.controller('RewardsController', [
 	'$scope', '$log', 'rewardsService', 'emojiService',
 	($scope, $log, $rewards, $emoji) => {
 		$scope.emoji = $emoji;
+		$scope.newReward = null;
 		$scope.fetchRewards = () => {
+			$scope.newReward = null;
 			$rewards.getAll().then((rewards) => {
+				$scope.newReward = angular.copy($rewards.reward({name: 'New Reward'}));
 				$scope.rewards = rewards.data;
 			});
 		};
